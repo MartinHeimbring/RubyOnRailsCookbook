857bdf67e84880465a909ab7ee502ed50378662a
Martin Heimbring <martin.heimbring@gmail.com>
2015-01-17 21:13:28 +0100

    create landing page controller + views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'landing_page/home'
+  root 'landing_page#home'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
